1756ae0d8d38e5fd7fa3e3a3b6fcab5b5bca818b
martholt <martholt@greeny.cs.tlu.ee>
2016-09-21 14:42:50 +0300

    Kodutöö valmis

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
-ï»¿<?php
-
+<?php
 
 	//var_dump($_GET);
 	
@@ -10,53 +9,73 @@
 	//var_dump($_POST);
 	
 	
-	
 	//kas keegi vajutas nuppu ja kas on olemas
 	
 	if (isset($_POST["signupEmail"])) {
 		//on olemas
-		// kas epost on tÃ¼hi
+		// kas epost on tühi
 		if (empty ($_POST["signupEmail"])) {
 			
-			//on tÃ¼hi
-			$signupEmailError = "VÃ¤li on kohustuslik!";
+			//on tühi
+			$signupEmailError = "Väli on kohustuslik!";
 		} 
 	}
 	
-	// kas epost on tÃ¼hi
+	// kas epost on tühi sssss
 	
 	
 	$signupPasswordError = "";
 	
 	//var_dump($_POST);
-	
-	
-	
-	
+
+
 	//kas keegi vajutas nuppu ja kas on olemas
 	
 	if (isset($_POST["signupPassword"])) {
 		//on olemas
-		// kas epost on tÃ¼hi
+		// kas epost on tühi
 		if (empty ($_POST["signupPassword"])) {
-			
-			//on tÃ¼hi
-			$signupPasswordError = "VÃ¤li on kohustuslik!";
+
+			//on tühi
+			$signupPasswordError = "Väli on kohustuslik!";
 		} else {
 			
-			//parool ei olnud tÃ¼hi
+			//parool ei olnud tühi
 			
 			if( strlen($_POST["signupPassword"]) < 8) {
 				
-				$signupPasswordError = "* Parrol peab olema vÃ¤hemalt 8 tÃ¤hemÃ¤rki pikk";
+				$signupPasswordError = "* Parrol peab olema vähemalt 8 tähemärki pikk";
 			}
 		}
 	}
-	
-	
-	
-	
-	
+
+	// SURENAME CONTROLL
+
+	$signupSurnameError = "";
+
+		if (isset($_POST["signupSurname"])) {
+		//on olemas
+		// kas epost on tühi
+		if (empty ($_POST["signupSurname"])) {
+
+			//on tühi
+			$signupSurnameError = "Väli on kohustuslik!";
+		}
+	}
+
+	// FORENAME CONTROLL
+	$signupForenameError = "";
+
+		if (isset($_POST["signupForename"])) {
+		//on olemas
+		// kas epost on tühi
+		if (empty ($_POST["signupForename"])) {
+
+			//on tühi
+			$signupForenameError = "Väli on kohustuslik!";
+		}
+	}
+
 ?>
 
 <!DOCTYPE html>
@@ -70,32 +89,26 @@
 
 	<form method="POST">
 		<input name="loginEmail" placeholder="E-post" type="email">
-		
 			<br/><br/>
-			
 		<input name="loginPassword" placeholder="Parool" type="password">
-		
 			<br/><br/>
-			
 		<input type="submit" value="Logi sisse">
 			<br/><br/>
 			<br/><br/>
-		
 	</form>
 	
 <h1>Loo kasutaja</h1>
 
 	<form method="POST">
 		<input name="signupEmail" placeholder="E-post" type="email"> <?php echo $signupEmailError; ?>
-		
 			<br/><br/>
-			
 		<input name="signupPassword" placeholder="Parool" type="password"> <?php echo $signupPasswordError; ?>
-		
 			<br/><br/>
-			
+		<input name="signupSurname" placeholder="First name " type="text"> <?php echo $signupSurnameError; ?>
+			<br/><br/>
+		<input name="signupForename" placeholder="Last name" type="text"> <?php echo $signupForenameError; ?>
+			<br/><br/>
 		<input type="submit" value="Loo Kasutaja">
-		
 	</form>
 </body>
 </html>
